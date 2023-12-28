c5e8c9519b011d3489ebccf39b120b22c5adaa39
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-12-28 14:30:31 +0500

    feat: some changes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,11 +4,25 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/HomePage.vue') },
+      { path: 'about', component: () => import('pages/AboutPage.vue') },
+      {
+        path: 'category/:id',
+        component: () => import('pages/CategoryPage.vue'),
+      },
+      {
+        path: 'product/:id',
+        component: () => import('pages/ProductDetail.vue'),
+      },
+      {
+        path: 'category/:id/product/:id',
+        component: () => import('pages/ProductDetail.vue'),
+      },
+      { path: 'shopping', component: () => import('pages/ShoppingPage.vue') },
+    ],
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
